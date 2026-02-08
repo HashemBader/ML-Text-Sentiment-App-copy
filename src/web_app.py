c70899e49970502a867963e7739dafabfd352fa6
Hashem Bader <hlbader@upei.ca>
2026-02-07 22:06:54 -0400

    compatible with render

--- src/web_app.py
+++ src/web_app.py
@@ -102,4 +102,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.getenv("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port, debug=False)
